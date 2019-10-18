836a8815ca29929d3a6042c6f9af54a533cc4e8f
github username <github email address>
2019-10-18 10:56:35 -0500

    time

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,45 @@
 class HeroinesController < ApplicationController
+  before_action :current_heroine, only: [:show, :edit, :update, :destroy]
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    @powers = Power.all
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:error] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def update
+    @heroine.update(heroine_params)
+    redirect_to heroine_path(@heroine)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def current_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,45 @@
 class PowersController < ApplicationController
+  before_action :current_heroine, only: [:show, :edit, :update, :destroy]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+  def new
+    @power = Power.new
+    @powers = Power.all
+  end
+
+  def create
+    @power = Power.new(power_params)
+    @powers = Power.all
+    if @power.valid?
+      @power.save
+      redirect_to power_path(@power)
+    else
+      flash[:error] = @power.errors.full_messages
+      redirect_to new_power_path
+    end
+  end
+
+  def update
+    @power.update(power_params)
+    redirect_to power_path(@power)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def current_heroine
+    @power = Power.find(params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description)
   end
 end
