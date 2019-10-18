758ecf92415a19bb25b19b70f2f2d7ff19d291ae
Damini varu <daminivaru@Daminis-MacBook-Pro.local>
2019-10-18 10:56:26 -0500

    I know this

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,28 @@
 Heroine.destroy_all
 Power.destroy_all
 
+
+heroines = [
+  {name: "Kamala Khan", super_name: "Ms. Marvel", power_id: "power.id"},
+  {name: "Doreen Green", super_name: "Squirrel Girl", power_id: "power.id"},
+  {name:"Gwen Stacy", super_name:"Spider-Gwen", power_id: "power.id" },
+  {name:"Janet Van Dyne", super_name:"The Wasp", power_id: "power.id" },
+  {name:"Wanda Maximoff", super_name:"Scarlet Witch", power_id: "power.id" },
+  {name:"Carol Danvers", super_name:"Captain Marvel", power_id: "power.id" },
+  {name:"Jean Grey", super_name:"Dark Phoenix", power_id: "power.id" },
+  {name:"Ororo Munroe", super_name:"Storm", power_id: "power.id" },
+  {name:"Kitty Pryde", super_name:"Shadowcat", power_id: "power.id" },
+  {name:"Elektra Natchios", super_name:"Elektra", power_id: "power.id" }
+]
+
 powers = [
-  {name:"super strength", description: "gives the wielder super-human strengths" },
-  {name:"flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" },
-  {name:"super human senses", description: "allows the wielder to user her senses at a super-human level" },
-  {name:"elasticity", description: "can stretch the human body to extreme lengths" }
+  {name:"super strength", description: "gives the wielder super-human strengths"},
+  {name:"flight", description: "gives the wielder the ability to fly through the skies at supersonic speed"},
+  {name:"super human senses", description: "allows the wielder to user her senses at a super-human level"},
+  {name:"elasticity", description: "can stretch the human body to extreme lengths"}
 ]
 
 powers.each {|power| Power.create(power)}
-
-heroines = [
-  {name: "Kamala Khan", super_name: "Ms. Marvel"},
-  {name: "Doreen Green", super_name: "Squirrel Girl" },
-  {name:"Gwen Stacy", super_name:"Spider-Gwen" },
-  {name:"Janet Van Dyne", super_name:"The Wasp" },
-  {name:"Wanda Maximoff", super_name:"Scarlet Witch" },
-  {name:"Carol Danvers", super_name:"Captain Marvel" },
-  {name:"Jean Grey", super_name:"Dark Phoenix" },
-  {name:"Ororo Munroe", super_name:"Storm" },
-  {name:"Kitty Pryde", super_name:"Shadowcat" },
-  {name:"Elektra Natchios", super_name:"Elektra" }
-]
 
 # ---------------
 # POWERFUL HEROINES:
