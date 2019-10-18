8aec628f764d335c77d6ce606d00807a02f1b7e0
Fabi <fafi@Fabis-MacBook-Pro.local>
2019-10-18 10:59:11 -0500

    -local

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,38 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroines = Heroine.find(params[:id])
+        @power = @heroines.powers
+  end
+
+  def new
+    @heroines = Heroine.new
+    @heroines.save
+  end
+
+  def edit
+    @heroines = Heroine.find([:id])
+  end
+
+  def create
+    @heroines = Heroine.create(heroines_params)
+    @powers = Power.all 
+  end
+  
+  def update
+      @heroine.update(heroines_params)
+      redirect_to @heroine
+  end
+  
+  def destroy
+    @heroines = Heroine.find(params[:id])
+    @heroines.destroy 
+    redirect_to @heroines
+  end 
+
+  def current_power
+      @heroine= Power.find(params[:id])
+  end
 end
