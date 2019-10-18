549d7472c02896e3e512e339afd014106bd983f8
chandonporter <cneilporter94@gmail.com>
2019-10-18 10:59:10 -0500

    Done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,48 @@
 class PowersController < ApplicationController
+
   def index
     @powers = Power.all
-  end
+    end
+    
+    def show
+        @power = Power.find(params[:id])
+        @heroines = @power.heroines
+    end
+
+    def edit 
+        @power = Power.find(params[:id])
+    end
+
+    def new
+        @power = Power.new
+    end
+
+    def create
+        @power = Power.new(power_params)
+        if @power.valid?
+            @power.save
+            redirect_to power_path(@power)
+        else
+            flash[:error] = @power.errors.full_messages
+            redirect_to new_power_path
+        end
+    end
+
+    def update
+     @power = Power.find(params[:id])
+     @power.update(power_params)
+     redirect_to heroine_path (@power)
+    end
+
+    def destroy
+     @power = Power.find(params[:id])
+     @power.destroy
+        redirect_to powers_path
+    end
+
+    private 
+
+    def power_params
+        params.require(:power).permit(:name)
+    end
 end
--- db/migrate/20191018143657_create_powers.rb
+++ db/migrate/20191018143657_create_powers.rb
@@ -1,9 +1,8 @@
-class CreatePowers < ActiveRecord::Migration[5.0]
+class CreatePowers < ActiveRecord::Migration[5.1]
   def change
     create_table :powers do |t|
       t.string :name
       t.string :description
-
       t.timestamps
     end
   end
